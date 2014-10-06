2ae4c88a573f328b34e13e32e14a35852921b934
Marco Asbreuk <marco@itsasbreuk.nl>
2014-10-06 23:30:46 +0200

    initial commit

---
+++ tests/window-ext.js
@@ -0,0 +1,13 @@
+/*global describe, it */
+"use strict";
+var expect = require('chai').expect,
+    should = require('chai').should();
+
+
+describe('Testgroup', function () {
+
+    it('Your test', function () {
+        // don't know how to setup a test
+    });
+
+});
